903ee0f5ff1ab5fc5443146bd26eef43754f55d5
Clfreeman22 <clfreeman22@gmail.com>
2021-10-30 11:50:27 -0500

    uncommented

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const inputCheck = require('./utils/inputCheck');
 
 // Express Middleware 
 app.use(express.urlencoded({ extended:false }));
@@ -60,17 +61,29 @@ const db = mysql.createConnection(
         });
       });
 
-// Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
-//               VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
 
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+// Create a candidate
+    app.post('/api/candidate', ({ body }, res) => {
+        const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+        if (errors) {
+            res.status(400).json({ error: errors });
+        return;
+        }
+        const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+      
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+        return;
+        }
+        res.json({
+          message: 'success',
+          data: body
+        });
+      });
+    });
 
 // Delete a candidate
     app.delete('/api/candidate/:id', (req, res) => {
